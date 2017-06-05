e68c782e672c33dd99f0f32e80d440ed747ee869
huzunrong <intern_huzunrong@cvte.com>
2017-06-05 16:52:06 +0800

    init

--- src/main/java/cn/flylinran/controller/IndexController.java
+++ src/main/java/cn/flylinran/controller/IndexController.java
@@ -1,6 +1,7 @@
 package cn.flylinran.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/index")
-    public String index() {
+    public String index(ModelMap map) {
+        map.addAttribute("name", "xiaohu");
         return "index";
     }
 }
